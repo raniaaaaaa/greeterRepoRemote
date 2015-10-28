61316b4a49cba7e0a55528c61eeb4b490b93c250
rania <r.benlahouane@gmail.com>
2015-10-28 10:35:44 +0100

    first commuit
    Signed-off-by: rania <r.benlahouane@gmail.com>

---
+++ greeter/src/test/java/edu/esprit/greeter/GreeterTest.java
@@ -0,0 +1,31 @@
+package edu.esprit.greeter;
+
+
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GreeterTest {
+	Greeter greeter;
+	@Before
+	public void Setup(){
+		greeter=new Greeter();
+	}
+	@Test
+	public void itShouldSayHelloWorld(){
+		String result = greeter.sayHello("world");
+		Assert.assertEquals("Hello world", result);
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void itshouldRaiseException(){
+		String param= "MAN";
+		greeter.sayHello(param);
+	}
+	@After 
+	public void tearDown(){
+		
+	}
+	
+}
